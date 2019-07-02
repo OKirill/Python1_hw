4a59ca42c7f8d7cb48c7f1699e989706c06587d5
Kyrylo Oliinyk <hotasice41@gmail.com>
2019-07-02 13:41:37 +0200

    Выполнил easy

---
+++ Homework1/easy.py
@@ -0,0 +1,29 @@
+# def age_check():
+#     age = input("Введите свой возраст: ")
+#     age = int(age)
+#     if age >= 18:
+#         print("Доступ разрешен")
+#     else:
+#         print("Извините, пользование данным ресурсом только с 18 лет")
+#
+# age_check()
+
+# Задание 1
+# a = "some string"
+# b = 2
+# c = "string № " + str(b)
+# d = input("Введите своё имя: ")
+#
+# print("Привет, " + d)
+# print(a)
+# print(b)
+# print(c)
+
+# Задание 2
+
+def number_plus():
+    number = input("Введите любое число: ")
+    number = int(number)
+    print(number + 2)
+
+number_plus()
