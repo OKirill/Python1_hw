37402c011c928c9901ce02819541240e0e4593e4
Kyrylo Oliinyk <hotasice41@gmail.com>
2019-07-03 14:45:30 +0200

    hard2

--- Homework1/hard.py
+++ Homework1/hard.py
@@ -4,12 +4,15 @@ age = input("Введите свой возраст:")
 weight = input("Введите свой вес: ")
 age = int(age)
 weight = int(weight)
-if age < 30:
-    if 50 < weight < 120:
-        print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- хорошее состояние")
-elif age > 30:
-    if 50 > weight > 120:
-        print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- следует заняться собой")
+if age <= 30 and 50 <= weight < 120:
+    print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- хорошее состояние")
+
+elif age > 40 and (weight < 50 or weight > 120):
+    print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- К Врачу")
+
+elif age > 30 and (weight < 50 or weight > 120):
+    print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- следует заняться собой!")
 
 else:
-    print(first_name + " " + last_name + ", ", age, "год, ", "вес", weight, "- К Врачу!")
+    print("Извиние вы не попадаете ни под какую категорию")
+
