e80de8d48d29cf676aeec6e782e06b25c3d5a61f
Kyrylo Oliinyk <hotasice41@gmail.com>
2019-07-09 13:57:58 +0200

    Закончил дз 3

--- Homework3/normal.py
+++ Homework3/normal.py
@@ -1,7 +1,16 @@
 employees = ['Vasya', 'Stepan', 'Jhon', 'Jack']
 salary = [55000, 720000, 120000, 63000]
-
+taxes = 0.13
 result = dict(zip(employees, salary))
 
 print(result)
+with open('salary.txt', 'w', encoding='utf-8') as file:
+    for key, value in result.items():
+        file.write(f'{key} - {value}\n')
 
+with open('salary.txt', 'r', encoding='utf-8') as file:
+    for line in file.readlines():
+        employee, single_salary = line.strip().split(' - ')
+        single_salary = int(single_salary)
+        if single_salary < 500_000:
+            print(f'{employee.upper()} - salary {single_salary - single_salary * taxes}')
